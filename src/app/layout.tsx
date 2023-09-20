5a02fa535ca2b1d0fbf867d5f8e94fa00a2e36ae
Yoichi Kobayashi <yoichi_kobayashi@yayoi-kk.co.jp>
2023-09-20 16:58:48 +0900

    moved global css file.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import "./globals.css";
+import "@/src/common/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { GlobalFooter } from "@/src/components/ui";

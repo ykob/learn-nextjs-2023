31a61871f28254d38209d983465dd4bce101b4b7
Yoichi Kobayashi <yoichi_kobayashi@yayoi-kk.co.jp>
2023-09-20 10:25:00 +0900

    added the rocket images on page.

--- src/app/spacex/rockets/[id]/page.tsx
+++ src/app/spacex/rockets/[id]/page.tsx
@@ -9,10 +9,23 @@ export default function SpaceXRocket({ params }: { params: { id: string } }) {
   if (isLoading) return <div>loading...</div>;
   if (!data) return <div>Data is not found.</div>;
 
+  const images = data.flickr_images ? (
+    data.flickr_images.map((o, i) => {
+      return (
+        <div key={`${data.id}-image-${i}`}>
+          <img src={o} alt="" width="480" />
+        </div>
+      );
+    })
+  ) : (
+    <></>
+  );
+
   return (
     <>
       <h2>{data.name}</h2>
-      <div>{data.id}</div>
+      <p>{data.description}</p>
+      <div>{images}</div>
     </>
   );
 }
--- src/common/types/spacex.d.ts
+++ src/common/types/spacex.d.ts
@@ -1,5 +1,7 @@
 type Rocket = {
   id: string;
+  description: string;
+  flickr_images: string[];
   name: string;
 };
 type Ship = {

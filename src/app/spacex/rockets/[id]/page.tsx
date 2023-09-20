9ab1126d5a468c33f95b7c24bba2347be8b7bd25
Yoichi Kobayashi <yoichi_kobayashi@yayoi-kk.co.jp>
2023-09-20 14:10:51 +0900

    added FetchRocketList on rocket page.

--- src/app/spacex/rockets/[id]/page.tsx
+++ src/app/spacex/rockets/[id]/page.tsx
@@ -1,5 +1,11 @@
-import { FetchRocketBody } from "@/src/components/functional";
+import { FetchRocketBody, FetchRocketList } from "@/src/components/functional";
 
 export default function SpaceXRocket({ params }: { params: { id: string } }) {
-  return <FetchRocketBody id={params.id} />;
+  return (
+    <>
+      <FetchRocketBody id={params.id} />
+      <h2>Space X Rockets</h2>
+      <FetchRocketList />
+    </>
+  );
 }
